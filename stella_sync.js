8001f42a3dc0c710f3d4c3d6437d5106f4af7726
Didier Prophete <didier.prophete@gmail.com>
2021-11-24 08:54:47 -0800

    just using node now

--- stella_sync.js
+++ stella_sync.js
@@ -1,33 +1,33 @@
-#!/usr/bin/env zx
-// usage: stella_sync.mjs --img <path to img> [--server url]
-//        stella_sync.mjs --dir <path to dir to watch> [--server url]
-//        stella_sync.mjs --port <port>
+#!/usr/bin/env node
+// usage: stella_sync.mj --img <path to img> [--server url]
+//        stella_sync.mj --dir <path to dir to watch> [--server url]
+//        stella_sync.mj --port <port>
 
-import "zx";
-import "zx/globals";
-$.verbose = false;
-
+const fs = require("fs-extra");
 const chokidar = require("chokidar");
 const express = require("express");
 const multer = require("multer");
+const chalk = require("chalk");
+const argv = require("minimist")(process.argv.slice(2));
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const os = require("os");
+const path = require("path");
 
 const tmpDir = "/tmp/stella_sync";
 const plateSolveDir = `${tmpDir}/platesolve`;
 const uploadDir = `${tmpDir}/upload`;
 const lockFile = "/tmp/stella_sync.lock";
+const { exec } = require("child_process");
+const fovStella = 1;
 
-// on windows, wsl can't ping localhost (apparently the port mapping in only one way so we
-// have to resolve localhost in a different way)
-const localhost = (await $`hostname -s`).stdout.trim() + ".local";
-const stellariumApi = `http://${localhost}:8090/api`;
+// will be defined later
+let stellariumApi;
 
 const PI = Math.PI;
 const cos = Math.cos;
 const sin = Math.sin;
 const asin = Math.asin;
 const atan2 = Math.atan2;
-
-const fovStella = 1;
 
 function log(...args) {
   console.log(chalk.yellow(ppNow()), ...args);
@@ -36,6 +36,22 @@ function log(...args) {
 function logError(...args) {
   play("/System/Library/Sounds/Ping.aiff");
   console.log(chalk.red(ppNow(), ...args));
+}
+
+function exe(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) reject(error);
+      else resolve(stdout || stderr);
+    });
+  });
+}
+
+async function setupAsyncVars() {
+  // on windows, wsl can't ping localhost (apparently the port mapping in only one way so we
+  // have to resolve localhost in a different way)
+  let localhost = (await exe(`hostname -s`)).trim() + ".local";
+  stellariumApi = `http://${localhost}:8090/api`;
 }
 
 //--------------------------------------------------------------------------------
@@ -118,9 +134,9 @@ function ppJ2000([x, y, z]) {
 // misc
 //--------------------------------------------------------------------------------
 
-function play(sound) {
+async function play(sound) {
   if (fs.pathExistsSync("/usr/bin/afplay")) {
-    $`afplay ${sound}`;
+    await exe(`afplay ${sound}`);
   }
 }
 
@@ -128,8 +144,8 @@ function play(sound) {
 // return: [ra, dec] (degreees)
 async function getRaDegStella() {
   try {
-    let res = await $`curl -s ${stellariumApi}/main/view`;
-    let j2000 = JSON.parse(JSON.parse(res.stdout)["j2000"]);
+    let res = await exe(`curl -s ${stellariumApi}/main/view`);
+    let j2000 = JSON.parse(JSON.parse(res)["j2000"]);
     let [raDeg, decDeg] = j2000ToDeg(j2000);
     log(`stellarium at: ${ppJ2000(j2000)} -> ra: ${ppDeg(raDeg)}, dec: ${ppDeg(decDeg)}`);
     return [raDeg, decDeg];
@@ -147,8 +163,8 @@ async function plateSolve({ srcImg, raDegStella, decDegStella, fovStella, server
 
 async function remotePlateSolve({ srcImg, raDegStella, decDegStella, fovStella, server }) {
   log("sending img to remove server for platesolve");
-  let res = await $`curl -X POST -F "ra=${raDegStella}" -F "dec=${decDegStella}" -F "fov=${fovStella}" -F "img=@${srcImg}" ${server}/platesolve`;
-  let { success, angle, j2000, error } = JSON.parse(res.stdout);
+  let res = await exe(`curl -X POST -F "ra=${raDegStella}" -F "dec=${decDegStella}" -F "fov=${fovStella}" -F "img=@${srcImg}" ${server}/platesolve`);
+  let { success, angle, j2000, error } = JSON.parse(res);
   if (success) {
     log(`solved: ${ppJ2000(j2000)}`);
     log(`rotation: ${ppDeg(angle)}`);
@@ -165,17 +181,17 @@ async function localPlateSolve({ srcImg, raDegStella, decDegStella, fovStella })
   fs.copySync(srcImg, dstImg);
 
   // plate solve
-  let res = await $`solve-field --cpulimit 20 --ra=${raDegStella} --dec=${decDegStella} --radius=${fovStella} --no-plot ${dstImg}`;
+  let res = await exe(`solve-field --cpulimit 20 --ra=${raDegStella} --dec=${decDegStella} --radius=${fovStella} --no-plot ${dstImg}`);
 
   // extract result
-  const matchRaDec = res.stdout.match(/Field center: \(RA,Dec\) = \(([-]?\d+.\d+), ([-]?\d+.\d+)\) deg./);
+  const matchRaDec = res.match(/Field center: \(RA,Dec\) = \(([-]?\d+.\d+), ([-]?\d+.\d+)\) deg./);
   if (matchRaDec == null) throw "error: couldn't solve for ra/dec";
   const raDeg = parseFloat(matchRaDec[1]);
   const decDeg = parseFloat(matchRaDec[2]);
   let j2000 = degToJ2000(raDeg, decDeg);
   log(`solved: ra: ${ppDeg(raDeg)}, dec: ${ppDeg(decDeg)} -> ${ppJ2000(j2000)}`);
 
-  const matchAngle = res.stdout.match(/Field rotation angle: up is ([-]?\d+.\d+) degrees/);
+  const matchAngle = res.match(/Field rotation angle: up is ([-]?\d+.\d+) degrees/);
   if (matchAngle == null) throw "error: couldn't solve for angle";
   const angle = (180 - parseFloat(matchAngle[1])) % 360;
   log(`rotation: ${ppDeg(angle)}`);
@@ -186,8 +202,8 @@ async function localPlateSolve({ srcImg, raDegStella, decDegStella, fovStella })
 // move stellarium
 // params: angle (in degrees), j2000
 async function moveStellarium(angle, [x, y, z]) {
-  await $`curl -s -d 'position=[${x}, ${y}, ${z}]' ${stellariumApi}/main/focus`;
-  await $`curl -s -d "id=Oculars.selectedCCDRotationAngle&value=${angle}" ${stellariumApi}/stelproperty/set`;
+  await exe(`curl -s -d 'position=[${x}, ${y}, ${z}]' ${stellariumApi}/main/focus`);
+  await exe(`curl -s -d "id=Oculars.selectedCCDRotationAngle&value=${angle}" ${stellariumApi}/stelproperty/set`);
 }
 
 // process image:
@@ -210,7 +226,7 @@ async function processImg(srcImg, server) {
   try {
     let [angle, j2000] = await plateSolve({ srcImg, raDegStella, decDegStella, fovStella, server });
     await moveStellarium(angle, j2000);
-    play("/System/Library/Sounds/Purr.aiff");
+    await play("/System/Library/Sounds/Purr.aiff");
   } catch (e) {
     logError(e);
   }
@@ -254,7 +270,7 @@ async function startServer(port) {
     fs.removeSync(srcImg);
   });
 
-  const localIp = (await $`ipconfig getifaddr en0`).stdout.trim();
+  const localIp = (await exe(`ipconfig getifaddr en0`)).trim();
   app.listen(port, () => {
     log(`starting in server mode on http://${localIp}:${port}`);
   });
@@ -264,40 +280,42 @@ async function startServer(port) {
 // main
 //--------------------------------------------------------------------------------
 
-fs.removeSync(lockFile);
+async function main() {
+  fs.removeSync(lockFile);
 
-if (argv.server) {
-  log(`will use remote server at ${argv.server} for platesolving`);
-  // try to ping server
-  try {
-    await fetch(`${argv.server}/ping`);
-  } catch (_) {
-    logError(`server did not respond`);
-    process.exit();
-  }
-}
-
-if (argv.img) {
-  const img = cleanPath(argv.img);
-  processImg(img, argv.server);
-} else if (argv.dir) {
-  const dir = cleanPath(argv.dir);
-  if (!fs.pathExistsSync(dir)) {
-    logError(`dir ${argv.dir} not found -> abort`);
-    process.exit();
-  }
-  log(`watching dir ${ppPath(dir)}`);
-  chokidar.watch(dir).on("add", (path) => {
-    if (path.endsWith(".fit") || path.endsWith(".png") || path.endsWith(".jpg")) {
-      log(chalk.yellow("--------------------------------------------------------------------------------"));
-      processImg(path, argv.server);
+  await setupAsyncVars();
+  if (argv.server) {
+    log(`will use remote server at ${argv.server} for platesolving`);
+    // try to ping server
+    try {
+      await fetch(`${argv.server}/ping`);
+    } catch (_) {
+      logError(`server did not respond`);
+      process.exit();
     }
-  });
-} else if (argv.port) {
-  const port = parseInt(argv.port);
-  startServer(port);
-} else {
-  console.log(`usage:
+  }
+
+  if (argv.img) {
+    const img = cleanPath(argv.img);
+    processImg(img, argv.server);
+  } else if (argv.dir) {
+    const dir = cleanPath(argv.dir);
+    if (!fs.pathExistsSync(dir)) {
+      logError(`dir ${argv.dir} not found -> abort`);
+      process.exit();
+    }
+    log(`watching dir ${ppPath(dir)}`);
+    chokidar.watch(dir).on("add", (path) => {
+      if (path.endsWith(".fit") || path.endsWith(".png") || path.endsWith(".jpg")) {
+        log(chalk.yellow("--------------------------------------------------------------------------------"));
+        processImg(path, argv.server);
+      }
+    });
+  } else if (argv.port) {
+    const port = parseInt(argv.port);
+    startServer(port);
+  } else {
+    console.log(`usage:
   stella_sync.mjs --img <img to analyze> [--server <server url>]
   stella_sync.mjs --dir <dir to watch> [--server <server url>]
   stella_sync.mjs --port <port>
@@ -312,4 +330,7 @@ if (argv.img) {
       ./stella_sync.mjs --dir <sharpcap img dir> --server <the exact ip of the server>
     This will monitor the shapcap dir, send the images to the server for platesolving, 
     and then properly center/orient stellarium.`);
+  }
 }
+
+main();
