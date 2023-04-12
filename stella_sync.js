ccc36d7c21878a3c9ebeee30f02c85d4fef2ec99
Didier Prophete <didier.prophete@gmail.com>
2023-04-11 21:31:42 -0700

    removed server

--- stella_sync.js
+++ stella_sync.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const fs = require("fs-extra");
-const express = require("express");
-const multer = require("multer");
 const chalk = require("chalk");
 const argv = require("minimist")(process.argv.slice(2));
 const path = require("path");
@@ -48,7 +46,7 @@ async function getFovStella() {
     const jsonRes = JSON.parse(res);
     const lensIndex = jsonRes["Oculars.selectedLensIndex"]["value"];
     const ratios = [1, 2.5, 0.73, 0.66, 0.6, 0.54, 1.6, 2.5];
-    const ratio = ratios[lensIndex + 1] || 1; 
+    const ratio = ratios[lensIndex + 1] || 1;
     if (ratio == 1) log(`detected ${chalk.blue("no barlow or FR")}`);
     else if (ratio > 1) log(`detected barlow ${chalk.blue(ratio)}`);
     else log(`detected FR ${chalk.blue(ratio)}`);
@@ -63,7 +61,7 @@ async function getFovStella() {
 // return: [angle (degrees), j2000]
 async function plateSolve(params) {
   let startDate = new Date().getTime();
-  const [angle, raDeg, decDeg] = params.server ? await remotePlateSolve(params) : await localPlateSolve(params);
+  const [angle, raDeg, decDeg] = await localPlateSolve(params);
   let endDate = new Date().getTime();
   log(`platesolving took ${chalk.blue(((endDate - startDate) / 1000).toFixed(2) + "s")}`);
 
@@ -71,18 +69,6 @@ async function plateSolve(params) {
   log(`solved: ra: ${chalk.blue(ppDeg(raDeg))}, dec: ${chalk.blue(ppDeg(decDeg))} -> ${ppJ2000(j2000)}`);
   log(`rotation: ${chalk.blue(ppDeg(angle))}`);
   return [angle, j2000];
-}
-
-// send image to remote server for platesolving
-async function remotePlateSolve({ srcImg, raDegStella, decDegStella, searchRadius, fovCamera, server }) {
-  log("sending img to remote server for platesolve");
-  const dstImg = `${uploadDir}/tmp${path.extname(srcImg)}`;
-  fs.removeSync(dstImg);
-  fs.copySync(srcImg, dstImg);
-  let res = await exe(`curl -X POST -F "ra=${raDegStella}" -F "dec=${decDegStella}" -F "seach=${searchRadius}" -F "fov=${fovCamera}" -F "img=@${dstImg}" ${server}/platesolve`, true);
-  let { success, angle, raDeg, decDeg, error } = JSON.parse(res);
-  if (success) return [angle, raDeg, decDeg];
-  throw error;
 }
 
 async function localPlateSolve({ srcImg, raDegStella, decDegStella, searchRadius, fovCamera }) {
@@ -133,7 +119,7 @@ async function moveStellarium(angle, [x, y, z]) {
 // - check with stellarium where we are pointing
 // - use this to platesolve (within 1° of where stellarium is pointing)
 // - move stellarium to exactly where we are (position + rotation)
-async function processImg(img, searchRadius, fovCamera, server) {
+async function processImg(img, searchRadius, fovCamera) {
   log(`processing ${chalk.blue(ppPath(img))}`);
   if (fs.pathExistsSync(lockFile)) {
     logError(`lockFile ${lockFile} already exists -> abort`);
@@ -159,7 +145,7 @@ async function processImg(img, searchRadius, fovCamera, server) {
     log(`delete img ${jpg}`);
   }
   try {
-    let [angle, j2000] = await plateSolve({ srcImg, raDegStella, decDegStella, searchRadius, fovCamera, server });
+    let [angle, j2000] = await plateSolve({ srcImg, raDegStella, decDegStella, searchRadius, fovCamera });
     await moveStellarium(angle, j2000);
     await play("/System/Library/Sounds/Purr.aiff");
   } catch (e) {
@@ -168,7 +154,7 @@ async function processImg(img, searchRadius, fovCamera, server) {
   fs.removeSync(lockFile);
 }
 
-async function processDir(dir, searchRadius, fovCamera, server, pattern) {
+async function processDir(dir, searchRadius, fovCamera, pattern) {
   if (!fs.pathExistsSync(dir)) {
     logError(`dir ${dir} not found -> abort`);
     process.exit();
@@ -179,56 +165,24 @@ async function processDir(dir, searchRadius, fovCamera, server, pattern) {
     if (path == "") continue; // when you delete files
     log(chalk.yellow("--------------------------------------------------------------------------------"));
     await sleep(500); // make sure the file is fully written (seems that sharpcap takes a little bit of time)
-    await processImg(path, searchRadius, fovCamera, server);
+    await processImg(path, searchRadius, fovCamera);
   }
 }
-//--------------------------------------------------------------------------------
-// server
-//--------------------------------------------------------------------------------
 
-async function startServer(port) {
-  const app = express();
-  const upload_middleware = multer({ dest: downloadDir });
+function usage() {
+  console.log(`usage:
+  stella_sync.js --img <img to analyze> [options]
+  stella_sync.js --dir <dir to watch> [options]
+  stella_sync.js --port <port> [options]
 
-  app.get("/ping", (req, res) => {
-    log("received ping");
-    res.send("file received");
-  });
+options:
+  --pattern: pattern for files to watch (when dir is used), default "*test*.fit"
+  --radius: search radius in degrees, default 25
+  --fov: fov of the camera in degrees, default 1
 
-  app.post("/platesolve", upload_middleware.single("img"), async (req, res) => {
-    log(chalk.yellow("--------------------------------------------------------------------------------"));
-    log(`received platesolve for ${req.file.originalname} (size: ${Math.round(req.file.size / 1024)}kb)`);
-    const srcImg = `${downloadDir}/${req.file.originalname}`;
-    fs.moveSync(req.file.path, srcImg, { overwrite: true });
-    const raDegStella = parseFloat(req.body.ra);
-    const decDegStella = parseFloat(req.body.dec);
-    const searchRadius = parseFloat(req.body.search);
-    const fovCamera = parseFloat(req.body.fov);
-    try {
-      let [angle, raDeg, decDeg] = await localPlateSolve({ srcImg, raDegStella, decDegStella, searchRadius, fovCamera });
-      res.json({ success: true, angle, raDeg, decDeg });
-    } catch (e) {
-      logError(e);
-      res.json({ success: false, error: e.toString() });
-    }
-    fs.removeSync(srcImg);
-  });
-
-  const localIp = await exe(`ipconfig getifaddr en0`);
-  app.listen(port, () => {
-    log(`starting in server mode on http://${localIp}:${port}`);
-  });
-}
-
-async function pingServer(server) {
-  log(`will use remote server at ${server} for platesolving`);
-  // try to ping server
-  try {
-    await exe(`curl -s ${server}/ping`);
-  } catch (_) {
-    logError(`server did not respond`);
-    process.exit();
-  }
+example:
+  ./stella_sync.js --dir $ASTRO/asistudio`);
+  process.exit();
 }
 
 //--------------------------------------------------------------------------------
@@ -236,6 +190,8 @@ async function pingServer(server) {
 //--------------------------------------------------------------------------------
 
 async function main() {
+  if (argv.length == 0) usage();
+
   fs.ensureDirSync(tmpDir);
   fs.ensureDirSync(uploadDir);
   fs.ensureDirSync(downloadDir);
@@ -255,29 +211,14 @@ async function main() {
   log(`using search radius ${chalk.blue(searchRadius.toFixed(2))}`);
   log(`using pattern ${chalk.blue(pattern)}`);
 
-  const server = argv.server;
-  if (server) pingServer(server);
-
   if (argv.img) {
     const img = cleanPath(argv.img);
-    processImg(img, searchRadius, fovCamera, server);
+    processImg(img, searchRadius, fovCamera);
   } else if (argv.dir) {
     const dir = cleanPath(argv.dir);
-    processDir(dir, searchRadius, fovCamera, server, pattern);
-  } else if (argv.port) {
-    const port = parseInt(argv.port);
-    startServer(port);
+    processDir(dir, searchRadius, fovCamera, pattern);
   } else {
-    console.log(`usage:
-  stella_sync.js --img <img to analyze> [options]
-  stella_sync.js --dir <dir to watch> [options]
-  stella_sync.js --port <port> [options]
-
-options:
-  --pattern: pattern for files to watch (when dir is used), default "*test*.fit"
-  --radius: search radius in degrees, default 25
-  --fov: fov of the camera in degrees, default 1
-  --server: an optional server url`);
+    usage();
   }
 }
 
