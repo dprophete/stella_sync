5e529ae08be1025e1adf3725ed2fd64d1e80f872
Didier Prophete <didier.prophete@gmail.com>
2023-04-14 15:54:03 -0700

    now cropping

--- siril_fit.js
+++ siril_fit.js
@@ -8,30 +8,68 @@ const path = require("path");
 const { log, cleanPath, exe, logError } = require("utils");
 
 const sirilCli = cleanPath("~/bin/siril-cli");
+const tmpScriptPath = "/tmp/siril.script";
+const cropPct = 0.15;
 
-async function runSiril(dir, inputFileName) {
+async function runSirilScript(script) {
+  fs.writeFileSync(tmpScriptPath, script);
+  await exe(`${sirilCli} -s ${tmpScriptPath}`);
+  fs.removeSync(tmpScriptPath);
+}
+
+// run a siril script to extract image metadata
+async function getFitMeta(dir, inputFileName) {
+  const justFileName = inputFileName.replace(".fit", "");
+  const jsonPath = `${dir}/${justFileName}.json`;
+
+  await runSirilScript(`
+    requires 1.2.0
+    cd "${dir}"
+    jsonmetadata "${inputFileName}"`);
+
+  let json = JSON.parse(fs.readFileSync(jsonPath));
+  fs.removeSync(jsonPath);
+
+  let fields = {};
+  json.headers.forEach(({ key, value }) => {
+    fields[key] = value;
+  });
+  return fields;
+}
+
+function cropParams(width, height, pct) {
+  const startX = (pct * width).toFixed();
+  const startY = (pct * height).toFixed();
+  const endX = width - 2 * startX;
+  const endY = height - 2 * startY;
+
+  return `${startX} ${startY} ${endX} ${endY}`;
+}
+
+async function process(dir, inputFileName) {
   if (path.extname(inputFileName) != ".fit") return;
+
   log(`processing ${chalk.blue(inputFileName)}`);
 
   fs.ensureDirSync(`${dir}/siril/fits`);
   fs.ensureDirSync(`${dir}/siril/jpgs`);
-  const tmpScriptPath = `${dir}/siril.script`;
-  const baseFileName = inputFileName.replace(".fit", "");
-  const script = `
+  const justFileName = inputFileName.replace(".fit", "");
+  let meta = await getFitMeta(dir, inputFileName);
+  let width = parseInt(meta["NAXIS1"]);
+  let height = parseInt(meta["NAXIS2"]);
+
+  await runSirilScript(`
     requires 1.2.0
     cd "${dir}"
     load "${inputFileName}"
+    crop ${cropParams(width, height, cropPct)}
     rmgreen 1
     subsky -rbf -samples=75 -tolerance=2.0 -smooth=0.5
     fmedian 3x3 1
     mirrorx
     autostretch -linked -2.8 0.1
-    save "siril/fits/${baseFileName}"
-    savejpg "siril/jpgs/${baseFileName}" 99`;
-
-  fs.writeFileSync(tmpScriptPath, script);
-  await exe(`${sirilCli} -s ${tmpScriptPath}`);
-  fs.removeSync(tmpScriptPath);
+    save "siril/fits/${justFileName}"
+    savejpg "siril/jpgs/${justFileName}" 99`);
 }
 
 function usage() {
@@ -59,7 +97,7 @@ async function main() {
 
     log(`processing dir ${chalk.blue(dir)}`);
     for (fileName of fs.readdirSync(dir)) {
-      await runSiril(dir, fileName);
+      await process(dir, fileName);
     }
   } else if (argv.img) {
     const img = cleanPath(argv.img);
@@ -67,7 +105,7 @@ async function main() {
       logError(`image ${img} not found -> abort`);
       process.exit();
     }
-    await runSiril(path.dirname(img), path.basename(img));
+    await process(path.dirname(img), path.basename(img));
   } else {
     usage();
   }
